329291105dd29835212d22c2087a3b41c851a09c
elio.goettelmann <elio.goettelmann@gmail.com>
2018-12-16 21:24:14 +0100

    Sample is now working:
    - fixed config ports
    - created service
    - added README file

---
+++ README.md
@@ -0,0 +1,9 @@
+Spring-Kafka-Sample
+===================
+
+Before starting the app, Zookeeper and Kafka need to be started:
+
+```
+zkServer start
+kafka-server-start /usr/local/etc/kafka/server.properties
+```
--- src/main/java/lu/elio/sample/springkafkasample/SpringKafkaSampleApplication.java
+++ src/main/java/lu/elio/sample/springkafkasample/SpringKafkaSampleApplication.java
@@ -1,35 +1,14 @@
 package lu.elio.sample.springkafkasample;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class SpringKafkaSampleApplication implements ApplicationRunner {
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+public class SpringKafkaSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaSampleApplication.class, args);
     }
 
-    public void sendMessage(String msg) {
-        kafkaTemplate.send("tutorialspoint", msg);
-    }
-
-    @KafkaListener(topics = "tutorialspoint", groupId = "group-id")
-    public void listen(String message) {
-        System.out.println("Received Messasge in group - group-id: " + message);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        sendMessage("Hi Welcome to Spring For Apache Kafka");
-    }
 }
 
