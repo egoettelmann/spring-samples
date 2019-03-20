adb8ba6a43a53742a53167667d606d96f0b91bf7
Elio Goettelmann <elio.goettelmann@arhs-cube.com>
2019-03-20 14:38:12 +0100

    Added sample-spring-react module
    - minimal working example for React app with Typescript
    - build is managed by webpack

---
+++ sample-spring-react/src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import SampleComponent from './components/SampleComponent';
+
+ReactDOM.render(
+  <div>
+    <h1>Sample Spring React App</h1>
+    <SampleComponent/>
+  </div>,
+  document.getElementById('root')
+);
