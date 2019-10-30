9727b330eaa5eba3e95f2be14837b8df7571db27
elio.goettelmann <elio.goettelmann@gmail.com>
2019-10-30 02:07:46 +0100

    Models of GraphQL schema are now generated

--- sample-spring-graphql/src/main/java/lu/elio/sample/spring/graphql/resolvers/RootQueryResolver.java
+++ sample-spring-graphql/src/main/java/lu/elio/sample/spring/graphql/resolvers/RootQueryResolver.java
@@ -1,25 +1,44 @@
 package lu.elio.sample.spring.graphql.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import lu.elio.sample.spring.graphql.models.TestModel;
+import lu.elio.sample.spring.graphql.generated.Query;
+import lu.elio.sample.spring.graphql.generated.TestModel;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class RootQueryResolver implements GraphQLQueryResolver {
+public class RootQueryResolver implements GraphQLQueryResolver, Query {
 
-    public List<TestModel> getTestModel(int count, int offset) {
-        return Arrays.asList(new TestModel(){{
+    private static final List<TestModel> TEST_MODELS = new ArrayList<TestModel>() {{
+        this.add(new TestModel(){{
             this.setId(1);
             this.setTestField1("TEST_FIELD_1.1");
             this.setTestField2("TEST_FIELD_1.2");
-        }}, new TestModel() {{
+        }});
+        this.add(new TestModel(){{
             this.setId(2);
             this.setTestField1("TEST_FIELD_2.1");
             this.setTestField2("TEST_FIELD_2.2");
         }});
+        this.add(new TestModel(){{
+            this.setId(3);
+            this.setTestField1("TEST_FIELD_3.1");
+            this.setTestField2("TEST_FIELD_3.2");
+        }});
+    }};
+
+    @Override
+    public List<TestModel> getTestModels(Integer count, Integer offset) {
+        int startIndex = Math.min(offset, TEST_MODELS.size());
+        int endIndex = Math.min(offset + count, TEST_MODELS.size());
+        return TEST_MODELS.subList(startIndex, endIndex);
     }
 
+    @Override
+    public TestModel getTestModel(Integer index) {
+        int id = Math.min(index, TEST_MODELS.size());
+        return TEST_MODELS.get(id);
+    }
 }
